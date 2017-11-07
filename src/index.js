ce05cce2ce64fbe45af9cd7c37f5393e76110728
zhangling <zhangling@antiy.cn>
2017-11-07 15:30:15 +0800

    'first'

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import registerServiceWorker from './registerServiceWorker';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import App from './App';
+
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/" component={App}/>
+            <Route path="/" component={App}/>
+        </div>
+    </Router>, document.getElementById('root'));
+registerServiceWorker();
