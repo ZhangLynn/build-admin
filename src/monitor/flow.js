07a7036757255fb97592f736ae639726210daab8
ZhangLynn <zl8312070@sina.com>
2018-02-25 13:25:04 +0800

    监测页构建流量监测和威胁监测子页

---
+++ src/monitor/flow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+
+export default class Demo extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
