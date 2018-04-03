c2cd50ac03be68d2bd3cfaaf7710502e9a2dd104
ZhangLynn <zl8312070@sina.com>
2018-04-03 21:11:07 +0800

    增加plugins配置文件

---
+++ src/plugins.js
@@ -0,0 +1,6 @@
+export {default as Config} from './config';
+export {default as FlowCharts} from './plugins/flowcharts';
+export {default as Location} from './plugins/location';
+export {default as PieCharts } from './plugins/piecharts';
+export {default as Tag} from './plugins/tag'
+export {default as Tagconnect} from './plugins/tagconnect';
