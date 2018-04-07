8218c9fc82c29432344e903b7ffcea892bac3b77
ZhangLynn <zl8312070@sina.com>
2018-04-07 23:31:55 +0800

    将connect的装饰器模式改为普通函数模式，使用了mapStateToProps和mapDispatchToProps函数传递state和dispatch

--- src/login/login.js
+++ src/login/login.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { login,getUserData } from './login.redux'
 import {Redirect} from 'react-router-dom'
 
 // 两个reducers 每个reducers都有一个state
 // 合并reducers
-@connect(
-    state=>state,
-    {login, getUserData}
-)
+// @connect(
+//     state=>state,
+//     {login, getUserData}
+// )
 class Login extends React.Component{
     // constructor(props) {
     // 	super(props)
@@ -17,6 +18,8 @@ class Login extends React.Component{
     // 	}
     // }
     componentDidMount() {
+        console.log(this.props)
+        // console.log(this.state);
         this.props.getUserData()
         // axios.get('/data')
         // 	.then(res=>{
@@ -26,16 +29,23 @@ class Login extends React.Component{
         // 	})
     }
     render(){
+        const user=this.props.user;
         return (
             <div>
-                <h2>我的名字是{this.props.user},年龄{this.props.age}</h2>
-                { this.props.isAuth? <Redirect to='/' /> : null}
+                <h2>我的名字是{user.user},年龄{user.age}</h2>
+                { user.isAuth? <Redirect to='/' /> : null}
                 <h2>你没有权限，需要登录才能看</h2>
                 <button onClick={this.props.login}>登录</button>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    return { user: state }
+}
 
-export default Login
+function mapDispatchToProps(dispatch) {
+    return { login: ()=>{dispatch(login())},getUserData:()=>{dispatch(getUserData())} }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
 
