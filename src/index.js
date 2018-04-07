ea83c352ed5b57eb30d2c19aa432ad24808fbde8
ZhangLynn <zl8312070@sina.com>
2018-04-07 21:29:23 +0800

    重新编写index.js，增加redux配置

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import registerServiceWorker from './registerServiceWorker';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Route,
   Link,
+    Redirect,
+    Switch
 } from 'react-router-dom'
-
+import {auth} from './login/login.redux'
+import Login from './login/login'
 import MyLayout from './menu/myMenu'
 import './index.css'
-
+const store = createStore(auth)
 
 
 ReactDOM.render(
-    <Router>
-            <Route path='/' component={MyLayout}></Route>
-    </Router>, document.getElementById('root'));
+    (<Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/login' component={Login}></Route>
+                <Route path='/layout' exact component={MyLayout}></Route>
+                <Redirect to='/MyLayout'></Redirect>
+            </Switch>
+        </BrowserRouter>
+    </Provider>), document.getElementById('root'));
 registerServiceWorker();
